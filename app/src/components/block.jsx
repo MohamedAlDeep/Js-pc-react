3140ec7a932e477f5aeafa802c04768cd54eb834
ICE988 <mohamedsamir22446688@gmail.com>
2023-01-21 22:43:19 +0200

    added all files

--- app/src/components/block.jsx
+++ app/src/components/block.jsx
@@ -1,16 +1,25 @@
 import '../css/index.css'
 import Data from '../data/data.json'
-function Block(){
-
-
-
+function Block()
+{
   return(
-    <div className="container">
-      <h1> {title} </h1>
-      <p>{about}</p>
-      <sub>{year}</sub>   
-      <sub>{price}</sub>   
-    </div>
+      <>
+      {
+        Data.map(props => 
+          <div className="main">
+            <div className="container">
+              <div className='wraper'>
+                <h1>{props.title}</h1>
+                <p>{props.about}</p>
+                <sub>{props.year}</sub>
+                <sub>{props.price}$</sub>
+              </div>
+            </div>
+          </div>
+        )
+      }
+      </>
   )
 }
+
 export default Block
